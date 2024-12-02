86b523daec45f602342d469ec5134eec14ef584d
Akshaya-Projects-2024 <akshaya.projects.2024@gmail.com>
2024-12-02 15:59:39 +0530

    Changes

--- src/pages/Account/myAccount.js
+++ src/pages/Account/myAccount.js
@@ -79,15 +79,15 @@ const MyAccount = (props) => {
     [profile?.providerProfile?.providerBusiness?.services]
   );
 
-  const paymentCompleted = useMemo(
-    () =>
-      profile?.providerProfile?.subscription?.reduce(
-        (accumulator, currentValue) =>
-          accumulator + `${currentValue?.service} `,
-        ""
-      ),
-    [profile?.providerProfile?.subscription]
-  );
+  // const paymentCompleted = useMemo(
+  //   () =>
+  //     profile?.providerProfile?.subscription?.reduce(
+  //       (accumulator, currentValue) =>
+  //         accumulator + `${currentValue?.service} `,
+  //       ""
+  //     ),
+  //   [profile?.providerProfile?.subscription]
+  // );
 
   const profileStatus = useMemo(() => {
     const validProviderProfile = validateServiceProfile(profile);
